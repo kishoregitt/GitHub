524b248eabfcc3b89805d66668df5ed6e55180bd
kishoregitt <kavalankishore@gmail.com>
2022-08-15 10:45:45 +0530

    initial codes

---
+++ src/test/java/org/git/GitHub.java
@@ -0,0 +1,22 @@
+package org.git;
+
+public class GitHub {
+	
+	private void M1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void M2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void M3() {
+		// TODO Auto-generated method stub
+
+	}
+	private void M4() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
